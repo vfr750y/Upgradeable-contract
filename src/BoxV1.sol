2c1bc0d0fd2adea0dc5c71c274102c349fb1b80a
vfr750y <th91843@proton.me>
2025-10-29 21:33:18 +1300

    disable initializers

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,9 +2,15 @@
 // initial version of the contract
 pragma solidity ^0.8.18;
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrade-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
